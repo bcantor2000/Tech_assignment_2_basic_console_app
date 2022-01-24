0a96c11b1aa483389bbfc4838446c4ffabaa9ebb
Brian <Brian@DESKTOP-40DC83T>
2022-01-24 16:49:42 -0500

    Add project files.

---
+++ Tech_assignment_2_basic_console_app/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Brian Cantor
+//1/24/2022
+//Tech Assignmnet #2 - Basic Console App
+Console.WriteLine("One day I am looking down at the screen, next thing you know the screen is looking down at you" + "\n" + "-Brian Cantor");
